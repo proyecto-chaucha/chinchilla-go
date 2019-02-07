4c1aa55ea03930581b4e83cba2ee3742a2df894a
César <heellxz@gmail.com>
2019-02-07 13:12:38 -0300

    Handlers main.go
    Solo handlers en main, todo lo demás se movió para los otros archivos

--- main.go
+++ main.go
@@ -7,86 +7,27 @@ import (
   "strconv"
   "net/http"
   "html/template"
-  "strings"
   "github.com/gorilla/mux"
   "github.com/proyecto-chaucha/chinchilla-go/types"
   "github.com/proyecto-chaucha/chinchilla-go/functions"
 )
 
-type blockPage struct {
-  Container BlocksContainer
-  Page int64
-  PageNext int64
-  PagePrev int64
-}
-
-var apiURL string = "http://localhost:21662/rest"
-var maxBlocks int = 40
-
-type BlocksContainer struct {
-  Block []types.Block
-}
-
-func (container *BlocksContainer) AddItem(item types.Block) []types.Block {
-    container.Block = append(container.Block, item)
-    return container.Block
-}
-
-func getblocks(offset int) BlocksContainer {
-  blocks := BlocksContainer{[]types.Block{}}
-
-  target := functions.GetWeb(apiURL + "/blockcount.json")
-  height64, err := strconv.ParseInt(strings.TrimSpace(target), 10, 0)
-  if err != nil { log.Fatal(err) }
-  height := int(height64)
-
-  var off int
-  if offset > maxBlocks { off = int(offset) - maxBlocks } else { off = 0 }
-
-  if height - off - maxBlocks + 1 >= 0 {
-    for i := height - off; i >= height - off - maxBlocks + 1; i-- {
-      hashTarget := functions.GetWeb(apiURL + "/getblockhash/" + strconv.Itoa(i) + ".json")
-      hash := hashTarget
-
-      blockTarget := types.Block{}
-      functions.GetJSON(apiURL + "/block/" + hash + ".json", &blockTarget)
-
-      blockTime := time.Unix(blockTarget.Time, 0)
-      blockTarget.Datetime = blockTime.Format("02.01.2006 15:04:05")
-      blockTarget.Txcount = len(blockTarget.Tx)
-
-      blocks.AddItem(blockTarget)
-    }
+func indexHandler(w http.ResponseWriter, r *http.Request)  {
+  pageSTR := r.URL.Query().Get("page")
+  if pageSTR == "" {
+    pageSTR = "1"
   }
-  return blocks
-}
+  fmt.Println("> GET /" + pageSTR)
 
-func index(w http.ResponseWriter, r *http.Request)  {
-  fmt.Println("> GET /")
-  var page int64 = 1
-  blocks := getblocks(int(page))
+  page, err := strconv.ParseInt(pageSTR, 10, 64)
+  blocks := functions.GetBlocks(int(page)*functions.Maxblocks)
 
-  tmpl := template.Must(template.ParseFiles("templates/home.html"))
-  blockpage := blockPage{Container: blocks,
-                         Page: page,
-                         PageNext: page + 1,
-                         PagePrev: page - 1}
-  tmpl.Execute(w, blockpage)
-}
-
-func indexOffset(w http.ResponseWriter, r *http.Request)  {
-  vars := mux.Vars(r)
-  fmt.Println("> GET /" + vars["id"])
-  page, err := strconv.ParseInt(vars["id"], 10, 64)
-
-  blocks := getblocks(int(page)*maxBlocks)
-
-  if err != nil || len(blocks.Block) == 0 {
+  if err != nil || len(blocks.Block) == 0 || page <= 0 {
     fmt.Fprintln(w, "Page not found")
 
   } else {
     tmpl := template.Must(template.ParseFiles("templates/home.html"))
-    blockpage := blockPage{Container: blocks,
+    blockpage := types.BlockPage{Container: blocks,
                            Page: page,
                            PageNext: page + 1,
                            PagePrev: page - 1}
@@ -102,7 +43,10 @@ func blockHandler(w http.ResponseWriter, r *http.Request)  {
     fmt.Println("> GET block:", hash)
 
     var blockTarget = types.Block{}
-    functions.GetJSON(apiURL + "/block/" + hash + ".json", &blockTarget)
+    functions.GetJSON("/block/" + hash + ".json", &blockTarget)
+
+    blockTime := time.Unix(blockTarget.Time, 0)
+    blockTarget.Datetime = blockTime.Format("02.01.2006 15:04:05")
 
     tmpl := template.Must(template.ParseFiles("templates/block.html"))
     tmpl.Execute(w, blockTarget)
@@ -117,8 +61,9 @@ func txHandler(w http.ResponseWriter, r *http.Request)  {
   hash := vars["id"]
 
   if len(hash) == 64 {
+    fmt.Println("> GET tx:", hash)
     var txTarget = types.Tx{}
-    functions.GetJSON(apiURL + "/tx/" + hash + ".json", &txTarget)
+    functions.GetJSON("/tx/" + hash + ".json", &txTarget)
 
     tmpl := template.Must(template.ParseFiles("templates/tx.html"))
     tmpl.Execute(w, txTarget)
@@ -132,8 +77,7 @@ func main() {
   fmt.Println("SERVER STARTED :D")
 
   r := mux.NewRouter()
-  r.HandleFunc("/", index)
-  r.HandleFunc("/{id}", indexOffset)
+  r.HandleFunc("/", indexHandler)
   r.HandleFunc("/block/{id}", blockHandler)
   r.HandleFunc("/tx/{id}", txHandler)
 
