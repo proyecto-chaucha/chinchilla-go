e5c8e9829c2455b75755a53f996fbfc0aeddc9d8
César <heellxz@gmail.com>
2019-03-07 01:00:36 -0300

    Logging & templates

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-  "fmt"
   "log"
   "time"
   "strconv"
@@ -14,23 +13,22 @@ import (
 
 func indexHandler(w http.ResponseWriter, r *http.Request)  {
   pageSTR := r.URL.Query().Get("page")
-  if pageSTR == "" {
-    pageSTR = "1"
-  }
-  fmt.Println("> GET /" + pageSTR)
+  if pageSTR == "" { pageSTR = "1" }
+
+  log.Println("> GET /" + pageSTR)
 
   page, err := strconv.ParseInt(pageSTR, 10, 64)
   blocks := functions.GetBlocks(int(page)*functions.Maxblocks)
 
   if err != nil || len(blocks.Block) == 0 || page <= 0 {
-    fmt.Fprintln(w, "Page not found")
+    log.Println(w, "Page not found")
 
   } else {
-    tmpl := template.Must(template.ParseFiles("templates/home.html"))
+    tmpl := template.Must(template.ParseFiles("web/home.html"))
     blockpage := types.BlockPage{Container: blocks,
-                           Page: page,
-                           PageNext: page + 1,
-                           PagePrev: page - 1}
+                                 Page: page,
+                                 PageNext: page + 1,
+                                 PagePrev: page - 1}
     tmpl.Execute(w, blockpage)
   }
 }
@@ -40,7 +38,7 @@ func blockHandler(w http.ResponseWriter, r *http.Request)  {
   hash := vars["id"]
 
   if len(hash) == 64 {
-    fmt.Println("> GET block:", hash)
+    log.Println("> GET block:", hash)
 
     var blockTarget = types.Block{}
     functions.GetJSON("/block/" + hash + ".json", &blockTarget)
@@ -48,11 +46,11 @@ func blockHandler(w http.ResponseWriter, r *http.Request)  {
     blockTime := time.Unix(blockTarget.Time, 0)
     blockTarget.Datetime = blockTime.Format("02.01.2006 15:04:05")
 
-    tmpl := template.Must(template.ParseFiles("templates/block.html"))
+    tmpl := template.Must(template.ParseFiles("web/block.html"))
     tmpl.Execute(w, blockTarget)
 
   } else {
-    fmt.Fprintln(w, "Page not found")
+    log.Println(w, "Page not found")
  }
 }
 
@@ -61,20 +59,20 @@ func txHandler(w http.ResponseWriter, r *http.Request)  {
   hash := vars["id"]
 
   if len(hash) == 64 {
-    fmt.Println("> GET tx:", hash)
+    log.Println("> GET tx:", hash)
     var txTarget = types.Tx{}
     functions.GetJSON("/tx/" + hash + ".json", &txTarget)
 
-    tmpl := template.Must(template.ParseFiles("templates/tx.html"))
+    tmpl := template.Must(template.ParseFiles("web/tx.html"))
     tmpl.Execute(w, txTarget)
 
   } else {
-    fmt.Fprintln(w, "Page not found")
+    log.Println(w, "Page not found")
   }
 }
 
 func main() {
-  fmt.Println("SERVER STARTED :D")
+  log.Println("SERVER STARTED :D")
 
   r := mux.NewRouter()
   r.HandleFunc("/", indexHandler)
